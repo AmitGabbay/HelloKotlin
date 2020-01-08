53c96c37e8107f433cc36f9c9127a5c4a8f82622
Amit Gabbay <amit.gabbay1@gmail.com>
2020-01-08 18:09:38 +0200

    finished functions:7

--- src/AMS.kt
+++ src/AMS.kt
@@ -1,13 +1,31 @@
 import java.util.*
 
 fun main(args: Array<String>) {
-    dayOfWeek()
+    //val hour : Int  = args[0].toInt()
+    //println("${if(args[0].toDouble()<12) "Good Morning, Kotlin" else "Good Night"}")
+    var fortune : String?
+    var runs = 0
+    do {
+        fortune =getFortuneCookie()
+        runs++
+        println("Your Fortune is $fortune.")
+
+    } while (fortune!="d" && runs < 10)
+
 }
 
-fun dayOfWeek(){
-    when(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)){
+fun dayOfWeek() {
+    when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
         in 1..3 -> println("Weekday")
         else -> println("weekend")
     }
-
 }
+    fun getFortuneCookie() : String  {
+        val greetings = listOf("a", "b", "c", "d", "e")
+        print("Enter your birthday: ")
+        //val a = readLine()
+        var birthday  = readLine()?.toIntOrNull() ?: 1
+        birthday = birthday.rem(greetings.count())
+        return greetings[birthday]
+    }
+
