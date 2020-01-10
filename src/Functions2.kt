fb9609c32e3d5a7c9565f03b42b5e8fb4d48ed85
Amit Gabbay <amit.gabbay1@gmail.com>
2020-01-10 02:02:20 +0200

    Finished Functions!

--- src/Functions2.kt
+++ src/Functions2.kt
@@ -1,3 +1,6 @@
+import kotlin.math.roundToInt
+import kotlin.random.Random
+
 fun main(args: Array<String>) {
 //    var fortune: String
 //    for (i in 1..10) {
@@ -5,9 +8,11 @@ fun main(args: Array<String>) {
 //        println("\nYour fortune is: $fortune")
 //        if (fortune.contains("Take it easy")) break
 //    }
-    print("Enter your mood: ")
-    val mood = readLine()
-    println(whatShouldIDoToday(mood!!))
+//    print("Enter your mood: ")
+//    val mood = readLine()
+//    println(whatShouldIDoToday(mood!!))
+
+    repeat(10){gamePlay(rollDice3(7))}
 }
 
 fun whatShouldIDoToday(mood : String, weather : String = "Sunny", temperature : Int = 24) : String{
@@ -23,3 +28,13 @@ fun whatShouldIDoToday(mood : String, weather : String = "Sunny", temperature : 
 fun isHappyAndSunny(mood : String, weather : String) = mood == "happy" && weather == "Sunny"
 fun isSadRainyAndCold(mood : String, weather : String, temperature : Int) = mood == "sad" && weather == "rainy" && temperature == 0
 fun isFuckingHot(temperature: Int) = temperature > 35
+
+val rollDice = {numOfSides : Int ->if (numOfSides!=0) (Math.random()*12).roundToInt() else 0}
+val rollDice2 : (Int)->Int = {numOfSides ->if (numOfSides!=0) (Math.random()*12).roundToInt() else 0}
+
+val rollDice3= {numOfSides : Int ->if (numOfSides!=0) Random.nextInt(12)+1 else 0}
+
+fun gamePlay(roll: Int){
+    println(roll)
+}
+
