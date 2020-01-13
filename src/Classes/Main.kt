70888007b9a3fecd682f158a37379a9ed306d1ea
Amit Gabbay <amit.gabbay1@gmail.com>
2020-01-13 02:10:05 +0200

    Finished Classes:3

---
+++ src/Classes/Main.kt
@@ -0,0 +1,8 @@
+package Classes
+
+fun main(args: Array<String>){
+    val sp = Spice()
+    println("Name: ${sp.name} " +
+            "Heat: ${sp.heat}")
+
+}
---
+++ src/Classes/Spice.kt
@@ -0,0 +1,9 @@
+package Classes
+
+class Spice {
+
+    val name : String = "curry"
+    val spiciness: String = "mild"
+    val heat : Int
+        get() =  5
+}
