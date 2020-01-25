3d60395c98e12ce94989e90f1dc7b71ca4f6c33f
Amit Gabbay <amit.gabbay1@gmail.com>
2020-01-25 22:02:10 +0200

    Lesson5: finished!

--- src/Lesson5/buildings/Buildings.kt
+++ src/Lesson5/buildings/Buildings.kt
@@ -5,7 +5,7 @@ open class BasicBuildingMaterial(val numberNeeded: Int = 1)
 class Wood : BasicBuildingMaterial(4)
 class Brick : BasicBuildingMaterial(8)
 
-class Building<T : BasicBuildingMaterial>(val material: T) {
+class Building<out T : BasicBuildingMaterial>(val material: T) {
     val baseMaterialNeeded = 100
     val actualMaterialsNeeded
         get() = material.numberNeeded * baseMaterialNeeded
@@ -13,11 +13,28 @@ class Building<T : BasicBuildingMaterial>(val material: T) {
     fun build() {
         println("Type is ${material::class.simpleName} and $actualMaterialsNeeded pieces are needed.")
     }
-
 }
 
+fun bulidi(ba : Building<BasicBuildingMaterial>){println(ba.material.numberNeeded)}
+
+fun <T: BasicBuildingMaterial> isSmallBuilding(b : Building<T>) {
+    if (b.actualMaterialsNeeded< 500) println("Small") else println("Big")
+}
+
+
 fun main(args: Array<String>) {
-    val b = Building(Wood())
-    b.build()
+    //val b = Building(Wood())
+    //b.build()
+    //bulidi(b)
+    //isSmallBuilding(b)
     //Building(Wood()).build()
+
+    loop@ for (i in 1..10) {
+        for (j in 1..10) {
+            if (i > 5) break@loop
+            print("$i,$j  ")
+        }
+        println()
+    }
+
 }
